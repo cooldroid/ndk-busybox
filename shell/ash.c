497b80332467f5a8e96236de02d0cc010532863f
CoolDroid <amit.bagaria@gmail.com>
2018-10-07 11:46:31 +0530

    ash history
    Auto generated by ndk-box-kitchen

--- shell/ash.c
+++ shell/ash.c
@@ -14021,7 +14021,7 @@ procargs(char **argv)
 	int login_sh;
 
 	xargv = argv;
-	login_sh = xargv[0] && xargv[0][0] == '-';
+	login_sh = 1; /* = xargv[0] && xargv[0][0] == '-'; - make always true for Android */
 	arg0 = xargv[0];
 	/* if (xargv[0]) - mmm, this is always true! */
 		xargv++;
@@ -14193,7 +14193,7 @@ int ash_main(int argc UNUSED_PARAM, char **argv)
 		const char *hp;
 
 		state = 1;
-		read_profile("/etc/profile");
+		read_profile("/system/etc/profile");
  state1:
 		state = 2;
 		hp = lookupvar("HOME");
@@ -14231,6 +14231,9 @@ int ash_main(int argc UNUSED_PARAM, char **argv)
 		if (iflag) {
 			const char *hp = lookupvar("HISTFILE");
 			if (!hp) {
+#ifdef __ANDROID__
+				setvar("HISTFILE", "/mnt/sdcard/ash_history", 0);
+#else
 				hp = lookupvar("HOME");
 				if (hp) {
 					INT_OFF;
@@ -14240,6 +14243,7 @@ int ash_main(int argc UNUSED_PARAM, char **argv)
 					INT_ON;
 					hp = lookupvar("HISTFILE");
 				}
+#endif
 			}
 			if (hp)
 				line_input_state->hist_file = hp;
